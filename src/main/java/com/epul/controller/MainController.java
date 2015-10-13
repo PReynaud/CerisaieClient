86c24f0e56c938ce1f61daa8e6b859609ac33f60
PReynaud <pierre.reynaud@hotmail.fr>
2015-10-13 08:25:36 +0200

    Ajout de la bibliotheque jersey client et refactoring

--- src/main/java/com/epul/controller/MainController.java
+++ src/main/java/com/epul/controller/MainController.java
@@ -1,4 +1,4 @@
-package com.springapp.mvc;
+package com.epul.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -7,10 +7,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/")
-public class HelloController {
+public class MainController {
 	@RequestMapping(method = RequestMethod.GET)
 	public String printWelcome(ModelMap model) {
 		model.addAttribute("message", "Hello world!");
-		return "hello";
+		return "index";
 	}
 }
